52c7db5b2bf678f85125ad2ae36965c0e3b12a61
vrushabh <vrushabhp702@gmail.com>
2024-11-27 16:17:51 +0530

    finally dynamic value of are comming from navbarjson to navbarf

--- Client/src/Components/Navbar/Navbar.jsx
+++ Client/src/Components/Navbar/Navbar.jsx
@@ -15,69 +15,58 @@ const Navbar = () => {
   }
 
   return (
-    <nav class="navbar navbar-expand-lg navbar-light px-4 px-lg-5 py-3 py-lg-0">
-      <a href="" class="navbar-brand p-0">
-        <h1 class="text-primary">
-          <i class="fas fa-search-dollar me-3"></i>Stocker
+    <nav className="navbar navbar-expand-lg navbar-light px-4 px-lg-5 py-3 py-lg-0">
+      <a href={navbarData.navbar.brand.link} className="navbar-brand p-0">
+        <h1 className="text-primary">
+          <i className={navbarData.navbar.brand.icon}></i>
+          {navbarData.navbar.brand.name}
         </h1>
         {/* <!-- <img src="img/logo.png" alt="Logo"> --> */}
       </a>
       <button
-        class="navbar-toggler"
+        className="navbar-toggler"
         type="button"
         data-bs-toggle="collapse"
         data-bs-target="#navbarCollapse"
       >
-        <span class="fa fa-bars"></span>
+        <span className="fa fa-bars"></span>
       </button>
-      <div class="collapse navbar-collapse" id="navbarCollapse">
-        <div class="navbar-nav ms-auto py-0">
-          <a href="index.html" class="nav-item nav-link active">
-            Home
-          </a>
-          <a href="about.html" class="nav-item nav-link">
-            About
-          </a>
-          <a href="service.html" class="nav-item nav-link">
-            Services
-          </a>
-          <a href="blog.html" class="nav-item nav-link">
-            Blogs
-          </a>
-          <div class="nav-item dropdown">
-            <a href="#" class="nav-link" data-bs-toggle="dropdown">
-              <span class="dropdown-toggle">Pages</span>
-            </a>
-            <div class="dropdown-menu m-0">
-              <a href="feature.html" class="dropdown-item">
-                Our Features
+      <div className="collapse navbar-collapse" id="navbarCollapse">
+        <div className="navbar-nav ms-auto py-0">
+          {navbarData.navbar.navLinks.map((link, index) =>
+            link.submenu ? (
+              <div key={index} className="nav-item dropdown">
+                <a href="#" className="nav-link" data-bs-toggle="dropdown">
+                  <span className="dropdown-toggle">{link.label}</span>
+                </a>
+                <div className="dropdown-menu m-0">
+                  {link.submenu.map((submenuLink, subIndex) => (
+                    <a
+                      key={subIndex}
+                      href={submenuLink.link}
+                      className="dropdown-item"
+                    >
+                      {submenuLink.label}
+                    </a>
+                  ))}
+                </div>
+              </div>
+            ) : (
+              <a
+                key={index}
+                href={link.link}
+                className={`nav-item nav-link ${link.active ? "active" : ""}`}
+              >
+                {link.label}
               </a>
-              <a href="team.html" class="dropdown-item">
-                Our team
-              </a>
-              <a href="testimonial.html" class="dropdown-item">
-                Testimonial
-              </a>
-              <a href="offer.html" class="dropdown-item">
-                Our offer
-              </a>
-              <a href="FAQ.html" class="dropdown-item">
-                FAQs
-              </a>
-              <a href="404.html" class="dropdown-item">
-                404 Page
-              </a>
-            </div>
-          </div>
-          <a href="contact.html" class="nav-item nav-link">
-            Contact Us
-          </a>
+            )
+          )}
         </div>
         <a
-          href="#"
-          class="btn btn-primary rounded-pill py-2 px-4 my-3 my-lg-0 flex-shrink-0"
+          href={navbarData.navbar.ctaButton.link}
+          className="btn btn-primary rounded-pill py-2 px-4 my-3 my-lg-0 flex-shrink-0"
         >
-          Get Started
+          {navbarData.navbar.ctaButton.label}
         </a>
       </div>
     </nav>
